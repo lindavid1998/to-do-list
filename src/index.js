0a45aec00e5f4c0339aed72c065930a06d8979aa
David Lin <lindavid1998@gmail.com>
2022-11-24 08:29:14 -0800

    Implement ProjectList module pattern

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+const ProjectList = (() => {
+    let projects = [];
+
+    const add = (project) => {
+        projects.push(project)
+    }
+
+    const remove = (projectName) => {
+        projects = projects.filter(project => project.name != projectName);
+    }
+
+    const getProjects = () => projects
+
+    return {
+        add,
+        remove,
+        getProjects
+    }
+})();
