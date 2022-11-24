7656bcd3ca46f2c78bfedb97bdb4a97f23ea172a
David Lin <lindavid1998@gmail.com>
2022-11-24 11:14:23 -0800

    Implement Project class

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const ProjectList = (() => {
     let projects = [];
 
     const add = (project) => {
-        projects.push(project)
+        projects.push(project);
     }
 
     const remove = (projectName) => {
         projects = projects.filter(project => project.name != projectName);
     }
 
-    const getProjects = () => projects
+    const getProjects = () => projects;
 
     return {
         add,
@@ -19,3 +19,20 @@ const ProjectList = (() => {
         getProjects
     }
 })();
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = []; //array of objects
+    }
+
+    addTask(task) {
+        // task is instance of class Task
+        this.tasks.push(task);
+    }
+
+    removeTask(taskName) {
+        // taskName is string
+        this.tasks = this.tasks.filter(task => task.title != taskName);
+    }
+}
